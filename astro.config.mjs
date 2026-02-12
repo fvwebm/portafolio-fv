d0addac24e9e302483697cef423e238e6cee1fd5
fvwebm <fvwebm@gmail.com>
2026-02-12 17:26:16 -0400

    Fix: Cambiando definitivamente a modo estático para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
-/*import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  //output: 'server',
-  integrations: [tailwind()]
-});*/
-
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+// Configuración de salida estática (SSG)
+// Esto permite que el sitio se despliegue en cualquier lugar sin errores de adaptador.
 export default defineConfig({
-  // ¡ESTA ES LA LÍNEA QUE SOLUCIONA EL ERROR DE JSON!
-  // Le dice a Astro que se comporte como un servidor dinámico.
-  output: 'server', 
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
